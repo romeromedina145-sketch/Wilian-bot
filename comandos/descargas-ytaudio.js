b3cbf97052d7bb74e95d4cff5472a38158bfb4ce
romeromedina145-sketch <romeromedina145@gmail.com>
2026-06-13 20:19:46 -0500

    Update descargas-ytaudio.js
    Guardar

--- comandos/descargas-ytaudio.js
+++ comandos/descargas-ytaudio.js
@@ -73,7 +73,7 @@ run: async (conn, m, args, usedPrefix, commandName, text) => {
                 totalMinutes = parts[0]
             }
 
-            if (totalMinutes >= 45) {
+            if (totalMinutes >= 180) {
                 await conn.sendMessage(m.chat, {
                     react: {
                         text: '⚠️',
@@ -82,7 +82,7 @@ run: async (conn, m, args, usedPrefix, commandName, text) => {
                 })
 
                 return m.reply(
-                    `*${config.visuals.emoji2}* El audio es demasiado largo. El límite permitido es de 45 minutos.`
+                    `*${config.visuals.emoji2}* El audio es demasiado largo. El límite permitido es de 180 minutos.`
                 )
             }
 
